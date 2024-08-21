e2ad6e281118fbb84923f27e4cad1b8add9aa37e
Almaz Galimov <almazwork@gmail.com>
2024-08-21 17:37:50 +0500

    add MeetParticipant in meet models and in model_admin

--- src/apps/meets/admin.py
+++ src/apps/meets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from apps.meets.models import Category, Meet
+from apps.meets.models import Category, Meet, MeetParticipant
 
 
 @admin.register(Category)
@@ -10,16 +10,23 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
+class MeetParticipantInline(admin.TabularInline):
+    model = MeetParticipant
+    extra = 1
+
+
 @admin.register(Meet)
 class MeetAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "start_date", "start_time")
     list_display_links = ("title", "category", "start_time")
     search_fields = ("title", "category", "start_time", "end_time")
+    inlines = [MeetParticipantInline]
 
-#
-# @admin.register(MeetParticipant)
-# class MeetParticipantAdmin(admin.ModelAdmin):
-#     list_display = ("user", )
-#     list_display_links = ("user", )
-#     search_fields = ("user", )
+
+@admin.register(MeetParticipant)
+class MeetParticipantAdmin(admin.ModelAdmin):
+    list_display = ("user", "meet", "status")
+    list_display_links = ("user", "meet", "status")
+    search_fields = ("user", "meet")
+
 
--- src/apps/meets/models.py
+++ src/apps/meets/models.py
@@ -41,21 +41,38 @@ class Meet(models.Model):
     )
     start_date = models.DateField(default=timezone.now, verbose_name="Дата")
     start_time = models.TimeField(default=timezone.now, verbose_name="Время")
-    participant = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Участник")
+    participants = models.ManyToManyField(User, through="MeetParticipant", related_name="meets", verbose_name="Участники")
+
+    class Meta:
+        db_table = "meets"
+        verbose_name = "Мит"
+        verbose_name_plural = "Миты"
+        ordering = ["start_date", "start_time", "category", "title"]
+
+    def __str__(self):
+        return f"{self.title} - {self.start_date} {self.start_time}"
+
+
+class MeetParticipant(models.Model):
+    """
+    Промежуточная модель, связывающая миты и участников
+    """
+    meet = models.ForeignKey('Meet', on_delete=models.CASCADE, verbose_name="Мит")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Участник")
     status = models.CharField(max_length=10, choices=StatusChoice.choices, default=StatusChoice.PRESENT, verbose_name="Статус")
+
+    class Meta:
+        db_table = "meet_participants"
+        unique_together = ('meet', 'user')
+        verbose_name = "Участник мита"
+        verbose_name_plural = "Участники мита"
 
     @property
     def status_color(self):
         return StatusChoice.get_color(self.status)
 
-    class Meta:
-        db_table = "meets"
-        verbose_name = "Мит"
-        ordering = ["start_date", "start_time", "title"]
-        verbose_name_plural = "Миты"
-
     def __str__(self):
-        return f"{self.title} - {self.start_date} {self.start_time}"
+        return f"{self.user.username} - {self.status_color} на {self.meet.title}"
 
 
 
