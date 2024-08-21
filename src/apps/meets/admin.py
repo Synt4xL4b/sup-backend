3a61b154b2d003fcf20f814dd0de3429a5ada437
Almaz Galimov <almazwork@gmail.com>
2024-08-21 16:51:17 +0500

    change StatusChoice. Change models

--- src/apps/meets/admin.py
+++ src/apps/meets/admin.py
@@ -1,25 +1,25 @@
 from django.contrib import admin
 
-from .models import Category, Meet, MeetParticipant
+from apps.meets.models import Category, Meet
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_display_links = ('name',)
-    search_fields = ('name',)
+    list_display = ("name",)
+    list_display_links = ("name",)
+    search_fields = ("name",)
 
 
 @admin.register(Meet)
 class MeetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'start_date', 'start_time')
-    list_display_links = ('title', 'category', 'start_time')
-    search_fields = ('title', 'category', 'start_time', 'end_time')
+    list_display = ("title", "category", "start_date", "start_time")
+    list_display_links = ("title", "category", "start_time")
+    search_fields = ("title", "category", "start_time", "end_time")
 
-
-@admin.register(MeetParticipant)
-class MeetParticipantAdmin(admin.ModelAdmin):
-    list_display = ('user', 'meet')
-    list_display_links = ('user', 'meet')
-    search_fields = ('user', 'meet')
+#
+# @admin.register(MeetParticipant)
+# class MeetParticipantAdmin(admin.ModelAdmin):
+#     list_display = ("user", )
+#     list_display_links = ("user", )
+#     search_fields = ("user", )
 
--- src/apps/meets/models.py
+++ src/apps/meets/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
-from ..users.models import User
+from django.core.validators import RegexValidator
+
+from apps.users.models import User
+from apps.meets.choice_classes import StatusChoice
 
 
 class Category(models.Model):
@@ -9,53 +12,52 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=100, unique=True, verbose_name="Категория")
 
+    class Meta:
+        db_table = "category"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ['name']
 
 
 class Meet(models.Model):
     """
     Модель самих митов, связана с категорией
     """
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Категория")
-    title = models.CharField(max_length=20, verbose_name="Название")
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, verbose_name="Категория")
+    title = models.CharField(
+        max_length=20,
+        unique=True,
+        verbose_name="Название",
+        validators=[
+            RegexValidator(
+                regex=r"^[a-zA-Zа-яА-Я\s]*$",
+                message="Допускаются только буквы латиницы и кириллицы.",
+                code="invalid_name"
+            ),
+        ],
+    )
     start_date = models.DateField(default=timezone.now, verbose_name="Дата")
     start_time = models.TimeField(default=timezone.now, verbose_name="Время")
+    participant = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Участник")
+    status = models.CharField(max_length=10, choices=StatusChoice.choices, default=StatusChoice.PRESENT, verbose_name="Статус")
+
+    @property
+    def status_color(self):
+        return StatusChoice.get_color(self.status)
+
+    class Meta:
+        db_table = "meets"
+        verbose_name = "Мит"
+        ordering = ["start_date", "start_time", "title"]
+        verbose_name_plural = "Миты"
 
     def __str__(self):
         return f"{self.title} - {self.start_date} {self.start_time}"
 
-    class Meta:
-        verbose_name = 'Мит'
-        verbose_name_plural = 'Миты'
-        ordering = ['start_date', 'start_time', 'title']
 
-
-class MeetParticipant(models.Model):
-    """
-    Модель участников митов и статусов
-    """
-    STATUS_CHOICES = [
-        ('present', 'Присутствует'),
-        ('absent', 'Отсутствует'),
-        ('warned', 'Предупредил'),
-    ]
-
-    meet = models.ForeignKey(Meet, on_delete=models.PROTECT, verbose_name="Мит")
-    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Пользователь")
-
-    def __str__(self):
-        return f"{self.user.username} - {self.status} на {self.meet.title}"
-
-    class Meta:
-        verbose_name = 'Участник мита'
-        verbose_name_plural = 'Участники мита'
-        ordering = ['meet', 'user']
 
 
 
