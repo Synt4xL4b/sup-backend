60f1eade5cfec90f15e232ae98e19d24583a1718
Almaz Galimov <almazwork@gmail.com>
2024-08-30 14:32:38 +0500

    fix meet view

--- src/apps/meets/views.py
+++ src/apps/meets/views.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-from .models import Meet, MeetParticipant
+from .models import Category, Meet, MeetParticipant
 
 
-class MeetsView(TemplateView):
+class MeetsView(LoginRequiredMixin, TemplateView):
     template_name = "meets.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
         context["participants"] = MeetParticipant.objects.all()
         context["meets"] = Meet.objects.all()
         return context
